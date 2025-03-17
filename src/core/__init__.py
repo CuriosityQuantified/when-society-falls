4d2e8d3be2399281567a3bc9fa1981c616d3d28d
CuriosityQuantified <68568231+CuriosityQuantified@users.noreply.github.com>
2025-03-17 16:01:47 -0500

    Add core module

---
+++ src/core/__init__.py
@@ -0,0 +1,12 @@
+"""
+Core module for the game engine.
+
+This module provides the basic infrastructure for the game engine.
+"""
+
+__all__ = [
+    'application',
+    'config',
+    'game_clock',
+    'input_manager',
+]
